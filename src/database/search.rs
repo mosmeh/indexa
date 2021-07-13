43896c058b5dc252f837af27e8fb0de1052f713c
mosm <airman515@gmail.com>
2021-07-14 00:56:07 +0900

    database/search: refactor

--- src/database/search.rs
+++ src/database/search.rs
@@ -18,151 +18,236 @@ use thread_local::ThreadLocal;
 impl Database {
     pub fn search(&self, query: &Query, abort_signal: &Arc<AtomicBool>) -> Result<Vec<EntryId>> {
         if query.is_empty() {
-            self.match_all(query)
-        } else if query.match_path() {
-            self.match_path(query, abort_signal)
-        } else {
-            self.match_basename(query, abort_signal)
+            return self.filter_and_sort::<AllFilter>(query, abort_signal);
         }
+        if !query.match_path() {
+            return self.filter_and_sort::<BasenameFilter>(query, abort_signal);
+        }
+        if query.is_regex_enabled() {
+            return self.filter_and_sort::<RegexPathFilter>(query, abort_signal);
+        }
+        self.filter_and_sort::<PathFilter>(query, abort_signal)
     }
 
-    fn match_all(&self, query: &Query) -> Result<Vec<EntryId>> {
-        self.collect_hits(query, |(id, _)| Some(Ok(EntryId(id))))
-    }
-
-    fn match_basename(
+    fn filter_and_sort<F: Filter>(
         &self,
         query: &Query,
         abort_signal: &Arc<AtomicBool>,
     ) -> Result<Vec<EntryId>> {
+        let ctx = FilterContext {
+            database: self,
+            abort_signal,
+            regex: query.regex(),
+        };
+
+        let mut hits = if let Some(ids) = self.sorted_ids[query.sort_by()].as_ref() {
+            match query.sort_order() {
+                SortOrder::Ascending => F::ordered(&ctx, ids.into_par_iter().copied())?,
+                SortOrder::Descending => F::ordered(&ctx, ids.into_par_iter().rev().copied())?,
+            }
+        } else {
+            let mut hits = F::unordered(&ctx)?;
+
+            let compare_func = util::get_compare_func(query.sort_by());
+            let slice = hits.as_parallel_slice_mut();
+            match query.sort_order() {
+                SortOrder::Ascending => slice.par_sort_unstable_by(|a, b| {
+                    compare_func(&self.entry(EntryId(*a)), &self.entry(EntryId(*b)))
+                }),
+                SortOrder::Descending => slice.par_sort_unstable_by(|a, b| {
+                    compare_func(&self.entry(EntryId(*b)), &self.entry(EntryId(*a)))
+                }),
+            };
+
+            hits
+        };
+
+        if query.sort_dirs_before_files() {
+            hits.as_parallel_slice_mut().par_sort_by(|a, b| {
+                self.nodes[*b as usize]
+                    .is_dir
+                    .cmp(&self.nodes[*a as usize].is_dir)
+            });
+        }
+
+        Ok(hits.into_iter().map(EntryId).collect())
+    }
+}
+
+struct FilterContext<'d, 'a, 'r> {
+    database: &'d Database,
+    abort_signal: &'a Arc<AtomicBool>,
+    regex: &'r Regex,
+}
+
+trait Filter {
+    /// Returns filtered ids without changing an order.
+    fn ordered(ctx: &FilterContext, ids: impl ParallelIterator<Item = u32>) -> Result<Vec<u32>>;
+
+    /// Returns filtered ids in an arbitrary order.
+    fn unordered(ctx: &FilterContext) -> Result<Vec<u32>>;
+}
+
+struct AllFilter;
+
+impl Filter for AllFilter {
+    fn ordered(_: &FilterContext, ids: impl ParallelIterator<Item = u32>) -> Result<Vec<u32>> {
+        Ok(ids.collect())
+    }
+
+    fn unordered(ctx: &FilterContext) -> Result<Vec<u32>> {
+        let hits = (0..ctx.database.num_entries() as u32).collect();
+        Ok(hits)
+    }
+}
+
+struct BasenameFilter;
+
+impl Filter for BasenameFilter {
+    fn ordered(ctx: &FilterContext, ids: impl ParallelIterator<Item = u32>) -> Result<Vec<u32>> {
         // Since rust-lang/regex@e040c1b, regex library stopped using thread_local,
         // which had a performance impact on indexa.
         // We mitigate it by putting Regex in thread local storage.
         let regex_tls = ThreadLocal::new();
 
-        self.collect_hits(query, |(id, node)| {
-            if abort_signal.load(Ordering::Relaxed) {
+        ids.filter_map(|id| {
+            if ctx.abort_signal.load(Ordering::Relaxed) {
                 return Some(Err(Error::SearchAbort));
             }
 
-            let regex = regex_tls.get_or(|| query.regex().clone());
+            let regex = regex_tls.get_or(|| ctx.regex.clone());
+            let node = &ctx.database.nodes[id as usize];
             regex
-                .is_match(self.basename_from_node(node))
-                .then(|| Ok(EntryId(id)))
+                .is_match(ctx.database.basename_from_node(node))
+                .then(|| Ok(id))
         })
+        .collect()
     }
 
-    fn match_path(&self, query: &Query, abort_signal: &Arc<AtomicBool>) -> Result<Vec<EntryId>> {
-        let mut hits = Vec::with_capacity(self.nodes.len());
-        for _ in 0..self.nodes.len() {
-            hits.push(AtomicBool::new(false));
+    fn unordered(ctx: &FilterContext) -> Result<Vec<u32>> {
+        let regex_tls = ThreadLocal::new();
+
+        let nodes = &ctx.database.nodes;
+        (0..nodes.len() as u32)
+            .into_par_iter()
+            .zip(nodes.par_iter())
+            .filter_map(|(id, node)| {
+                if ctx.abort_signal.load(Ordering::Relaxed) {
+                    return Some(Err(Error::SearchAbort));
+                }
+
+                let regex = regex_tls.get_or(|| ctx.regex.clone());
+                regex
+                    .is_match(ctx.database.basename_from_node(node))
+                    .then(|| Ok(id))
+            })
+            .collect()
+    }
+}
+
+struct PathFilter;
+
+impl Filter for PathFilter {
+    fn ordered(ctx: &FilterContext, ids: impl ParallelIterator<Item = u32>) -> Result<Vec<u32>> {
+        let matched = Self::match_path(ctx)?;
+
+        ids.filter_map(|id| {
+            if ctx.abort_signal.load(Ordering::Relaxed) {
+                return Some(Err(Error::SearchAbort));
+            }
+
+            matched[id as usize].load(Ordering::Relaxed).then(|| Ok(id))
+        })
+        .collect()
+    }
+
+    fn unordered(ctx: &FilterContext) -> Result<Vec<u32>> {
+        let matched = Self::match_path(ctx)?;
+
+        (0..ctx.database.num_entries() as u32)
+            .into_par_iter()
+            .zip(matched.into_par_iter())
+            .filter_map(|(id, hit)| {
+                if ctx.abort_signal.load(Ordering::Relaxed) {
+                    return Some(Err(Error::SearchAbort));
+                }
+
+                hit.load(Ordering::Relaxed).then(|| Ok(id))
+            })
+            .collect()
+    }
+}
+
+impl PathFilter {
+    fn match_path(ctx: &FilterContext) -> Result<Vec<AtomicBool>> {
+        let nodes = &ctx.database.nodes;
+        let mut matched = Vec::with_capacity(nodes.len());
+        for _ in 0..nodes.len() {
+            matched.push(AtomicBool::new(false));
         }
 
         let regex_tls = ThreadLocal::new();
+        let root_paths = &ctx.database.root_paths;
 
-        if query.is_regex_enabled() {
-            for (root_id, root_path) in &self.root_paths {
-                let root_node = &self.nodes[*root_id as usize];
-                if query.regex().is_match(root_path.to_str().unwrap()) {
-                    hits[*root_id as usize].store(true, Ordering::Relaxed);
-                }
-
-                self.match_path_impl(root_node, root_path, query, &regex_tls, &hits, abort_signal)?;
-            }
-        } else {
-            for ((root_id, root_path), next_root_id) in self.root_paths.iter().zip(
-                self.root_paths
-                    .keys()
-                    .skip(1)
-                    .copied()
-                    .chain(std::iter::once(self.nodes.len() as u32)),
-            ) {
-                let root_node = &self.nodes[*root_id as usize];
-                if query.regex().is_match(root_path.to_str().unwrap()) {
-                    (*root_id..next_root_id)
-                        .into_par_iter()
-                        .try_for_each(|id| {
-                            if abort_signal.load(Ordering::Relaxed) {
-                                return Err(Error::SearchAbort);
-                            }
-                            hits[id as usize].store(true, Ordering::Relaxed);
-                            Ok(())
-                        })?;
-                } else {
-                    self.match_path_impl(
-                        root_node,
-                        root_path,
-                        query,
-                        &regex_tls,
-                        &hits,
-                        abort_signal,
-                    )?;
-                }
+        for ((root_id, root_path), next_root_id) in root_paths.iter().zip(
+            root_paths
+                .keys()
+                .skip(1)
+                .copied()
+                .chain(std::iter::once(nodes.len() as u32)),
+        ) {
+            if ctx.regex.is_match(root_path.to_str().unwrap()) {
+                matched[*root_id as usize..next_root_id as usize]
+                    .into_par_iter()
+                    .try_for_each(|hit| {
+                        if ctx.abort_signal.load(Ordering::Relaxed) {
+                            return Err(Error::SearchAbort);
+                        }
+                        hit.store(true, Ordering::Relaxed);
+                        Ok(())
+                    })?;
+            } else {
+                let root_node = &nodes[*root_id as usize];
+                Self::match_path_impl(ctx, root_node, root_path, &regex_tls, &matched)?;
             }
         }
 
-        self.collect_hits(query, |(id, _)| {
-            if abort_signal.load(Ordering::Relaxed) {
-                return Some(Err(Error::SearchAbort));
-            }
-
-            hits[id as usize]
-                .load(Ordering::Relaxed)
-                .then(|| Ok(EntryId(id)))
-        })
+        Ok(matched)
     }
 
     fn match_path_impl(
-        &self,
+        ctx: &FilterContext,
         node: &EntryNode,
         path: &Path,
-        query: &Query,
         regex_tls: &ThreadLocal<Regex>,
-        hits: &[AtomicBool],
-        abort_signal: &Arc<AtomicBool>,
+        matched: &[AtomicBool],
     ) -> Result<()> {
-        let regex = regex_tls.get_or(|| query.regex().clone());
+        let regex = regex_tls.get_or(|| ctx.regex.clone());
 
-        (node.child_start..node.child_end)
+        let children_range = node.child_start as usize..node.child_end as usize;
+        (
+            &ctx.database.nodes[children_range.clone()],
+            &matched[children_range],
+        )
             .into_par_iter()
-            .try_for_each(|id| {
-                if abort_signal.load(Ordering::Relaxed) {
+            .try_for_each(|(node, hit)| {
+                if ctx.abort_signal.load(Ordering::Relaxed) {
                     return Err(Error::SearchAbort);
                 }
 
-                let child = &self.nodes[id as usize];
-                let child_path = path.join(&self.basename_from_node(child));
+                let child_path = path.join(&ctx.database.basename_from_node(node));
                 if let Some(s) = child_path.to_str() {
-                    if query.is_regex_enabled() {
-                        if regex.is_match(s) {
-                            hits[id as usize].store(true, Ordering::Relaxed);
+                    if regex.is_match(s) {
+                        hit.store(true, Ordering::Relaxed);
+                        if node.has_any_child() {
+                            Self::match_all_descendants(ctx, node, matched)?;
                         }
+                        return Ok(());
+                    }
 
-                        if child.has_any_child() {
-                            self.match_path_impl(
-                                child,
-                                &child_path,
-                                query,
-                                regex_tls,
-                                hits,
-                                abort_signal,
-                            )?;
-                        }
-                    } else if regex.is_match(s) {
-                        hits[id as usize].store(true, Ordering::Relaxed);
-
-                        if child.has_any_child() {
-                            self.match_all_descendants(child, hits, abort_signal)?;
-                        }
-                    } else if child.has_any_child() {
-                        self.match_path_impl(
-                            child,
-                            &child_path,
-                            query,
-                            regex_tls,
-                            hits,
-                            abort_signal,
-                        )?;
+                    if node.has_any_child() {
+                        Self::match_path_impl(ctx, node, &child_path, regex_tls, matched)?;
+                        return Ok(());
                     }
                 }
 
@@ -171,74 +256,117 @@ impl Database {
     }
 
     fn match_all_descendants(
-        &self,
+        ctx: &FilterContext,
         node: &EntryNode,
-        hits: &[AtomicBool],
-        abort_signal: &Arc<AtomicBool>,
+        matched: &[AtomicBool],
     ) -> Result<()> {
-        (node.child_start..node.child_end)
+        let children_range = node.child_start as usize..node.child_end as usize;
+        (
+            &ctx.database.nodes[children_range.clone()],
+            &matched[children_range],
+        )
             .into_par_iter()
-            .try_for_each(|id| {
-                if abort_signal.load(Ordering::Relaxed) {
+            .try_for_each(|(node, hit)| {
+                if ctx.abort_signal.load(Ordering::Relaxed) {
                     return Err(Error::SearchAbort);
                 }
 
-                hits[id as usize].store(true, Ordering::Relaxed);
-
-                let child = &self.nodes[id as usize];
-                if child.has_any_child() {
-                    self.match_all_descendants(child, hits, abort_signal)?;
+                hit.store(true, Ordering::Relaxed);
+                if node.has_any_child() {
+                    Self::match_all_descendants(ctx, node, matched)?;
                 }
 
                 Ok(())
             })
     }
+}
 
-    fn collect_hits<F>(&self, query: &Query, func: F) -> Result<Vec<EntryId>>
-    where
-        F: Fn((u32, &EntryNode)) -> Option<Result<EntryId>> + Send + Sync,
-    {
-        let hits: Result<Vec<_>> = if let Some(sorted_ids) =
-            self.sorted_ids[query.sort_by()].as_ref()
-        {
-            let iter = sorted_ids
-                .par_iter()
-                .map(|id| (*id, &self.nodes[*id as usize]));
-            match query.sort_order() {
-                SortOrder::Ascending => iter.filter_map(func).collect(),
-                SortOrder::Descending => iter.rev().filter_map(func).collect(),
+struct RegexPathFilter;
+
+impl Filter for RegexPathFilter {
+    fn ordered(ctx: &FilterContext, ids: impl ParallelIterator<Item = u32>) -> Result<Vec<u32>> {
+        let matched = Self::match_path(ctx)?;
+
+        ids.filter_map(|id| {
+            if ctx.abort_signal.load(Ordering::Relaxed) {
+                return Some(Err(Error::SearchAbort));
             }
-        } else {
-            let mut v = (0..self.nodes.len() as u32)
-                .into_par_iter()
-                .zip(self.nodes.par_iter())
-                .filter_map(func)
-                .collect::<Result<Vec<_>>>()?;
 
-            let compare_func = util::get_compare_func(query.sort_by());
-            match query.sort_order() {
-                SortOrder::Ascending => v
-                    .as_parallel_slice_mut()
-                    .par_sort_unstable_by(|a, b| compare_func(&self.entry(*a), &self.entry(*b))),
-                SortOrder::Descending => v
-                    .as_parallel_slice_mut()
-                    .par_sort_unstable_by(|a, b| compare_func(&self.entry(*b), &self.entry(*a))),
-            };
+            matched[id as usize].load(Ordering::Relaxed).then(|| Ok(id))
+        })
+        .collect()
+    }
 
-            Ok(v)
-        };
+    fn unordered(ctx: &FilterContext) -> Result<Vec<u32>> {
+        let matched = Self::match_path(ctx)?;
 
-        if query.sort_dirs_before_files() {
-            hits.map(|mut hits| {
-                hits.as_parallel_slice_mut().par_sort_by(|a, b| {
-                    self.nodes[b.0 as usize]
-                        .is_dir
-                        .cmp(&self.nodes[a.0 as usize].is_dir)
-                });
-                hits
+        (0..ctx.database.num_entries() as u32)
+            .into_par_iter()
+            .zip(matched.into_par_iter())
+            .filter_map(|(id, hit)| {
+                if ctx.abort_signal.load(Ordering::Relaxed) {
+                    return Some(Err(Error::SearchAbort));
+                }
+
+                hit.load(Ordering::Relaxed).then(|| Ok(id))
             })
-        } else {
-            hits
+            .collect()
+    }
+}
+
+impl RegexPathFilter {
+    fn match_path(ctx: &FilterContext) -> Result<Vec<AtomicBool>> {
+        let nodes = &ctx.database.nodes;
+        let regex_tls = ThreadLocal::new();
+
+        let mut matched = Vec::with_capacity(nodes.len());
+        for _ in 0..nodes.len() {
+            matched.push(AtomicBool::new(false));
         }
+
+        for (root_id, root_path) in &ctx.database.root_paths {
+            if ctx.regex.is_match(root_path.to_str().unwrap()) {
+                matched[*root_id as usize].store(true, Ordering::Relaxed);
+            }
+
+            let root_node = &nodes[*root_id as usize];
+            Self::match_path_impl(ctx, root_node, root_path, &regex_tls, &matched)?;
+        }
+
+        Ok(matched)
+    }
+
+    fn match_path_impl(
+        ctx: &FilterContext,
+        node: &EntryNode,
+        path: &Path,
+        regex_tls: &ThreadLocal<Regex>,
+        matched: &[AtomicBool],
+    ) -> Result<()> {
+        let regex = regex_tls.get_or(|| ctx.regex.clone());
+
+        let children_range = node.child_start as usize..node.child_end as usize;
+        (
+            &ctx.database.nodes[children_range.clone()],
+            &matched[children_range],
+        )
+            .into_par_iter()
+            .try_for_each(|(node, hit)| {
+                if ctx.abort_signal.load(Ordering::Relaxed) {
+                    return Err(Error::SearchAbort);
+                }
+
+                let child_path = path.join(&ctx.database.basename_from_node(node));
+                if let Some(s) = child_path.to_str() {
+                    if regex.is_match(s) {
+                        hit.store(true, Ordering::Relaxed);
+                    }
+                    if node.has_any_child() {
+                        Self::match_path_impl(ctx, node, &child_path, regex_tls, matched)?;
+                    }
+                }
+
+                Ok(())
+            })
     }
 }
